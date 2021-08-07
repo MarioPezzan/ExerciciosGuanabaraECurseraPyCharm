e017a3e785bbae3fc1b4252eafdaf1f408a7e7c5
MarioPezzan <marinho.mariojose@users.noreply.github.com>
2021-08-07 00:10:49 -0300

    Versionamento exercícios

--- Exercícios curso em video/Exercicios/ex084.py
+++ Exercícios curso em video/Exercicios/ex084.py
@@ -1,26 +1,31 @@
-pessoas = []
+pessoasepeso = []
 dados = []
-contpessoas = maiorpeso = menorpeso = 0
+maiorpeso = menorpeso = 0
 nomemaior = []
 nomemenor = []
 while True:
     dados.append(str(input('Digite o nome: ')))
-    dados.append(int(input('Digite o peso: ')))
+    dados.append(float(input('Digite o peso: ')))
+    if len(pessoasepeso) == 0:
+        menorpeso = maiorpeso = dados[1]
+    if dados[1] < menorpeso:
+        menorpeso = dados[1]
+        nomemenor.append(dados[0])
+    if dados[1] > maiorpeso:
+        maiorpeso = dados[1]
+        nomemaior.append(dados[0])
     n = str(input('Deseja continuar? [S/N]: '))
-    pessoas.append(dados[:])
-    contpessoas += 1
+    pessoasepeso.append(dados[:])
     dados.clear()
     if n in 'Nn':
         break
-for p, c in enumerate(pessoas):
-    if p == 1:
-        menorpeso = pessoas[0][1]
-    if c[1] <= menorpeso:
-        menorpeso = c[1]
-        nomemenor.append(c[0])
-    if c[1] > maiorpeso:
-        maiorpeso = c[1]
-        nomemaior.append(c[0])
-print(f'A quantidade de pessoas registradas foi de {contpessoas}')
-print(f'O maior peso é de {maiorpeso}Kg. Peso de {nomemaior} ')
-print(f'O menor peso é de {menorpeso}Kg. Peso de {nomemenor}')
+print(f'A quantidade de pessoas registradas foi de {len(pessoasepeso)}')
+print(f'O maior peso é de {maiorpeso}Kg. Peso de', end=' ')
+for c in pessoasepeso:
+    if c[1] == maiorpeso:
+        print(c[0], end=' ')
+print()
+print(f'O menor peso é de {menorpeso}Kg. Peso de', end=' ')
+for c in pessoasepeso:
+    if c[1] == menorpeso:
+        print(c[0], end=' ')
